4262ce7bac7eebf86b21d9f8a6013a32e78f90c2
Ambrósia Andrade <70069811+ambrosiaandrade@users.noreply.github.com>
2025-07-11 14:09:04 -0300

    Factory method

--- src/main/java/design_patterns/factory_method/done/FactoryMethodMain.java
+++ src/main/java/design_patterns/factory_method/done/FactoryMethodMain.java
@@ -5,6 +5,10 @@ import config.MessageProvider;
 import design_patterns.factory_method.done.exercise1.EmailFactory;
 import design_patterns.factory_method.done.exercise1.PushFactory;
 import design_patterns.factory_method.done.exercise1.SMSFactory;
+import design_patterns.factory_method.done.exercise2.*;
+import design_patterns.factory_method.done.exercise3.FileReaderFactory;
+
+import java.util.*;
 
 public class FactoryMethodMain {
 
@@ -18,6 +22,11 @@ public class FactoryMethodMain {
         CustomPrint.colored("factoryMethod1");
         exercise1();
 
+        CustomPrint.colored("factoryMethod2");
+        exercise2();
+
+        CustomPrint.colored("factoryMethod3");
+        exercise3();
 
     }
 
@@ -31,8 +40,26 @@ public class FactoryMethodMain {
         push.notificarUsuario("Fulano mandou mensagem");
     }
 
-    private void out(Object o) {
-        System.out.println(o);
+    private static void exercise2() {
+        new FabricaDeGuerreiros().iniciarAventura();
+        new FabricaDeMagos().iniciarAventura();
+        new FabricaDeArqueiros().iniciarAventura();
+    }
+
+    private static void exercise3() {
+        List<String> fileNames = List.of("planilha.csv", "dados.json", "script.py", "dados.xml", "tcc.doc");
+
+        fileNames.forEach(name -> {
+            String extension = name.substring(name.indexOf("."));
+            FileReaderFactory factory = FileReaderFactory.handleFileType(extension);
+            if (factory == null) {
+                CustomPrint.error("There is no factory for file extension " + extension);
+                return;
+            }
+            System.out.format(":: INPUT :: %s%n", name);
+            factory.handleFile();
+        });
+
     }
 
 }
